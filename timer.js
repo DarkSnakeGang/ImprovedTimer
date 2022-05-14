8e84e322bb4055366cc36700e6f012b135c10bcb
fizhes <80635091+fizhes@users.noreply.github.com>
2022-05-14 09:20:18 -0400

    fixe again

--- timer.js
+++ timer.js
@@ -13,34 +13,16 @@ if(window.snake) {
         if(this.responseText.indexOf('trophy') === -1)
           return;
 
-        const poisonsnake = this.responseText.match(
-          /([a-zA-Z0-9_$]{1,8}\(this\.[a-zA-Z0-9_$]{1,8},"#5282F2","#909090"\);){3}/
-        )[0];
-
-        const poison = (window.snake_scheme_epic_cool || {}).custom_poison || '#909090';
-
+        
         eval(
           this.responseText.match(
-            /[a-zA-Z0-9_$]{1,8}\.prototype\.resetState=function\(a\){a=void 0===a\?!0:a;this\.direction="NONE";this\.[a-zA-Z0-9_$]{1,8}="RIGHT";[^]*?this,!1\)}/
+            /[a-zA-Z0-9_$]{1,8}\.prototype\.resetState=function\(a\){a=void 0===a\?!0:[^]*?this\.menu,!1,this\.[a-zA-Z0-9_$]{1,8}\.[a-zA-Z0-9_$]{1,8}\)}/
           )[0].replace(
             '--:--:---',
             '-'
-          ).replace(
-            '{',
-            `{
-              ${poisonsnake.replace(/#909090/g, poison)}
-            `
           )
         );
 
-        // eval(
-        //   this.responseText.match(
-        //     /[a-zA-Z0-9_$]{1,8}=function\(a\){if\(!a\.[a-zA-Z0-9_$]{1,8}&&![a-zA-Z0-9_$]{1,8}\)[^}]*?"show"\)\]\)}}/
-        //   )[0].match(/[a-zA-Z0-9_$]{1,8}/)[0] + `= snake.speedrun = function() {
-        //     const q = document.getElementsByClassName('A2vT0')[0].style.display;
-        //     document.getElementsByClassName('A2vT0')[0].style.display = q ? '' : 'none';
-        //   }`
-        // );
 
 
         eval(
